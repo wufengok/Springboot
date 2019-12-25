6e6dc697d579d65902086987ef092b6561c96bb6
wufengok <872567693@qq.com>
2019-12-25 20:23:10 +0800

    .

--- src/main/java/com/study/MQ_Springboot/direct/SenderConfig.java
+++ src/main/java/com/study/MQ_Springboot/direct/SenderConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Slf4j
-public class RabbitConfig {
+public class SenderConfig {
 
     @Bean
     public RabbitTemplate createRabbitTemplate(ConnectionFactory connectionFactory){
