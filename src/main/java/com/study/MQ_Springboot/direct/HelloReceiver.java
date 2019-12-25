3389f10de75c351fa61d01a739286f8e796f5e5b
wufengok <872567693@qq.com>
2019-12-25 20:17:37 +0800

    .

--- src/main/java/com/study/MQ_Springboot/direct/HelloReceiver.java
+++ src/main/java/com/study/MQ_Springboot/direct/HelloReceiver.java
@@ -21,20 +21,16 @@ public class HelloReceiver implements ChannelAwareMessageListener {
     public void onMessage(Message message, Channel channel) throws Exception {
         long deliveryTag = message.getMessageProperties().getDeliveryTag();
         try {
-            //因为传递消息的时候用的map传递,所以将Map从Message内取出需要做些处理
-            String msg = message.toString();
-            String[] msgArray = msg.split("'");//可以点进Message里面看源码,单引号直接的数据就是我们的map消息数据
-            Map<String, String> msgMap = mapStringToMap(msgArray[1].trim());
-            String messageId=msgMap.get("messageId");
-            String messageData=msgMap.get("messageData");
-            String createTime=msgMap.get("createTime");
-
-            log.info("messageId:"+messageId+"  messageData:"+messageData+"  createTime:"+createTime);
+            String messageStr = new String(message.getBody());
             log.info("===============================");
+
+            //用于肯定确认
             channel.basicAck(deliveryTag, true);
-//			channel.basicReject(deliveryTag, true);//为true会重新放回队列
+
         } catch (Exception e) {
-            channel.basicReject(deliveryTag, false);
+            //用于否定确认
+            channel.basicNack(deliveryTag,false,true);
+//			channel.basicReject(deliveryTag, true);//为true会重新放回队列
             e.printStackTrace();
         }
     }
@@ -54,7 +50,7 @@ public class HelloReceiver implements ChannelAwareMessageListener {
 
     @RabbitHandler
     public void process(String hello) {
-
+        //int i = 1/0;
         log.error("Receiver  : " + hello);
         log.error("////////////////////////////////////////////////////////////////////\n" +
                 "//                          _ooOoo_                               //\n" +
