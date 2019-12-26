fea50d0e352655a1f2444682452b56076fe698a6
wufengok <872567693@qq.com>
2019-12-26 15:56:24 +0800

    .

--- src/main/java/com/study/MQ_Springboot/direct/HelloReceiver.java
+++ src/main/java/com/study/MQ_Springboot/direct/HelloReceiver.java
@@ -6,17 +6,28 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
-@RabbitListener(queues = "queue_direct")
-@Slf4j   //
-public class HelloReceiver implements ChannelAwareMessageListener {
+@Slf4j   // implements ChannelAwareMessageListener
+public class HelloReceiver  {
 
     @RabbitHandler
+    @RabbitListener(queues = "queue_direct")
+    public void receive(Message message, Channel channel) throws IOException {
+        try {
+            //int i = 1/0;
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+            System.out.println("接受到的消息为"+new String(message.getBody(),"UTF-8"));
+        } catch (Exception e) {
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(),false,true);
+        }
+    }
+
     public void process(String hello) {
-        //int i = 1/0;
+        int i = 1/0;
 
         log.info("Receiver  : " + hello);
         log.info("////////////////////////////////////////////////////////////////////\n" +
@@ -49,7 +60,7 @@ public class HelloReceiver implements ChannelAwareMessageListener {
         System.out.println("=============================================================================================================================================");
     }
 
-    @Override
+    /*@Override
     public void onMessage(Message message, Channel channel) throws Exception {
         long deliveryTag = message.getMessageProperties().getDeliveryTag();
         try {
@@ -67,5 +78,5 @@ public class HelloReceiver implements ChannelAwareMessageListener {
 
             e.printStackTrace();
         }
-    }
+    }*/
 }
